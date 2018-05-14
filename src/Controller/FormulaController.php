c80dc420e5237772e812ca4edcbfe4a50fee2295
Giancarlos Salas <giansalex@gmail.com>
2018-05-13 21:38:34 -0500

    add product pages

--- src/Controller/FormulaController.php
+++ src/Controller/FormulaController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Formula;
-use App\Form\FormulaType;
 use App\Repository\FormulaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,18 +2,49 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/product")
+ */
 class ProductController extends Controller
 {
     /**
-     * @Route("/product", name="product_index")
+     * @Route("/", name="product_index", methods="GET")
+     * @param ProductRepository $productRepository
+     * @return Response
      */
-    public function index()
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+        return $this->render('product/index.html.twig', ['products' => $productRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="product_new", methods="GET|POST")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $product = new Product();
+
+        return $this->render('product/new.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="product_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Product $product): Response
+    {
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
         ]);
     }
 }
