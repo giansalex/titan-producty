ac0d38635c837576acdc854c445a848e9b1b543b
Giancarlos Salas <giansalex@gmail.com>
2018-05-13 21:42:41 -0500

    add production pages

--- src/Controller/ProductionController.php
+++ src/Controller/ProductionController.php
@@ -2,18 +2,50 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Production;
+use App\Form\ProductionType;
+use App\Repository\ProductionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/production")
+ */
 class ProductionController extends Controller
 {
     /**
-     * @Route("/production", name="production_index")
+     * @Route("/", name="production_index", methods="GET")
+     * @param ProductionRepository $productionRepository
+     * @return Response
      */
-    public function index()
+    public function index(ProductionRepository $productionRepository): Response
     {
-        return $this->render('production/index.html.twig', [
-            'controller_name' => 'ProductionController',
+        return $this->render('production/index.html.twig', ['productions' => $productionRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="production_new", methods="GET|POST")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $production = new Production();
+
+        return $this->render('production/new.html.twig', [
+            'production' => $production,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="production_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Production $production): Response
+    {
+        return $this->render('production/edit.html.twig', [
+            'production' => $production,
         ]);
     }
 }
