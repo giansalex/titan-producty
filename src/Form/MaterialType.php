050e3a01dd277db7ca78142fb328247e29bbfcd4
Giancarlos Salas <giansalex@gmail.com>
2018-05-13 21:43:53 -0500

    add material crud

---
+++ src/Form/MaterialType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Material;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MaterialType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('code')
+            ->add('amount')
+            ->add('unit')
+            ->add('price')
+            ->add('packingPrice')
+            ->add('stock')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Material::class,
+        ]);
+    }
+}
