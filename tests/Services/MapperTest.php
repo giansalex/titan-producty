f48322b92a23b308f5eace78a226038f71ca775d
Giancarlos Salas <giansalex@gmail.com>
2018-05-20 16:25:49 -0500

    add map Array test with fake

--- tests/Services/MapperTest.php
+++ tests/Services/MapperTest.php
@@ -11,6 +11,8 @@ namespace App\Tests\Services;
 use App\Dto\FormulaDto;
 use App\Entity\Formula;
 use App\Services\Mapper;
+use Faker\Factory;
+use Faker\Generator;
 use PHPUnit\Framework\TestCase;
 
 class MapperTest extends TestCase
@@ -19,19 +21,27 @@ class MapperTest extends TestCase
      * @var Mapper
      */
     private $mapper;
+    /**
+     * @var Generator
+     */
+    private $faker;
 
     protected function setUp()
     {
         $this->mapper = new Mapper();
+        $this->faker = Factory::create('es_ES');
     }
 
-    public function testMap()
+    public function testMap(): void
     {
+        $gen = $this->faker;
+
         $formula = new Formula();
-        $formula->setName('MAT 1')
-            ->setAmount(2.44)
-            ->setUnit('mm')
-            ->setNotes('-----');
+        $formula
+            ->setName($gen->text)
+            ->setAmount($gen->randomFloat())
+            ->setUnit($gen->text)
+            ->setNotes($gen->text);
 
         /**@var $dto FormulaDto */
         $dto = $this->mapper->map($formula, FormulaDto::class);
@@ -42,5 +52,26 @@ class MapperTest extends TestCase
         $this->assertEquals($formula->getNotes(), $dto->notes);
         $this->assertTrue(is_string($dto->name));
         $this->assertTrue(is_float($dto->amount));
+    }
+
+    public function testMapArray(): void
+    {
+        $gen = $this->faker;
+        $formula = new Formula();
+        $formula
+            ->setName($gen->text)
+            ->setAmount($gen->randomFloat())
+            ->setUnit($gen->text)
+            ->setNotes($gen->text);
+
+        $dtos = $this->mapper->mapArray([$formula], FormulaDto::class);
+
+        $this->assertEquals(1, count($dtos));
+        $dto = $dtos[0];
+        $this->assertEquals(FormulaDto::class, get_class($dto));
+        $this->assertEquals($formula->getName(), $dto->name);
+        $this->assertEquals($formula->getAmount(), $dto->amount);
+        $this->assertEquals($formula->getUnit(), $dto->unit);
+        $this->assertEquals($formula->getNotes(), $dto->notes);
     }
 }
