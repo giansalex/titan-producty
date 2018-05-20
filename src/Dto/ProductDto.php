5f76f8edf05e0b05bb6cf049c4e79058e3df5b0a
Giancarlos Salas <giansalex@gmail.com>
2018-05-20 16:33:12 -0500

    add product dto

---
+++ src/Dto/ProductDto.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giansalex
+ * Date: 20/05/2018
+ * Time: 16:28
+ */
+
+namespace App\Dto;
+
+/**
+ * Class ProductDto
+ */
+class ProductDto
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $code;
+
+    /**
+     * @var float
+     */
+    public $amount;
+
+    /**
+     * @var string
+     */
+    public $unit;
+
+    /**
+     * @var string
+     */
+    public $notes;
+
+    /**
+     * @var float
+     */
+    public $baseAmount;
+
+    /**
+     * @var int
+     */
+    public $formulaId;
+}
