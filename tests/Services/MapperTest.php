98f56aee6ca324a7cbcd05cead910384ff6e81c4
giansalex <giansalex@gmail.com>
2018-05-20 10:53:33 -0500

    add mapper test

---
+++ tests/Services/MapperTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Atenea
+ * Date: 20/05/2018
+ * Time: 10:35
+ */
+
+namespace App\Tests\Services;
+
+use App\Dto\FormulaDto;
+use App\Entity\Formula;
+use App\Services\Mapper;
+use PHPUnit\Framework\TestCase;
+
+class MapperTest extends TestCase
+{
+    /**
+     * @var Mapper
+     */
+    private $mapper;
+
+    protected function setUp()
+    {
+        $this->mapper = new Mapper();
+    }
+
+    public function testMap()
+    {
+        $formula = new Formula();
+        $formula->setName('MAT 1')
+            ->setAmount(2.44)
+            ->setUnit('mm')
+            ->setNotes('-----');
+
+        /**@var $dto FormulaDto */
+        $dto = $this->mapper->map($formula, FormulaDto::class);
+
+        $this->assertEquals($formula->getName(), $dto->name);
+        $this->assertEquals($formula->getName(), $dto->amount);
+        $this->assertEquals($formula->getName(), $dto->unit);
+        $this->assertEquals($formula->getName(), $dto->notes);
+    }
+}
