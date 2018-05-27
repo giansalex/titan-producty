f2b3d3d44c1ae5226702fde95b7a48c89730f93a
Giancarlos Salas <giansalex@gmail.com>
2018-05-27 14:09:01 -0500

    add get, edit to service, ctrl

--- assets/js/app/material/material.service.js
+++ assets/js/app/material/material.service.js
@@ -9,16 +9,21 @@
     function materialService($http) {
         return {
             add: add,
+            get: get,
             edit: edit,
             list: list,
         };
+
+        function get(id) {
+            return $http.get(Routing.generate('material_api_get', {id: id}));
+        }
 
         function add(material) {
             return $http.post(Routing.generate('material_api_add'), material);
         }
 
         function edit(id, material) {
-            return $http.put(Routing.generate('material_api_edit'), material);
+            return $http.put(Routing.generate('material_api_edit', {id: id}), material);
         }
 
         function list() {
--- assets/js/app/material/new.controller.js
+++ assets/js/app/material/new.controller.js
@@ -9,17 +9,38 @@
     function newController($material, $window) {
         const vm = this;
         vm.material = {};
+        vm.get = get;
         vm.create = create;
+        vm.edit = edit;
 
         activate();
 
         function activate() {
         }
 
+        function get(id) {
+            $material.get(id)
+                .then(function (r) {
+                    vm.material = r.data;
+                });
+        }
+
         function create() {
             const material = vm.material;
 
             $material.add(material)
+                .then(successAdded);
+
+            function successAdded() {
+                $window.location.href = Routing.generate('material_index');
+            }
+        }
+
+        function edit(id) {
+            const material = vm.material;
+            console.log(material);
+
+            $material.edit(id, material)
                 .then(successAdded);
 
             function successAdded() {
