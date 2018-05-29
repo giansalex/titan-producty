129648c259da7fe452c665b7c5c14475b3664fd5
Giancarlos Salas <giansalex@gmail.com>
2018-05-28 22:20:53 -0500

    show action production

--- src/Controller/ProductionController.php
+++ src/Controller/ProductionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Production;
 use App\Repository\ProductionRepository;
 use App\Services\Ensure;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -54,5 +56,19 @@ class ProductionController extends Controller
     public function edit(): Response
     {
         return $this->render('production/edit.html.twig');
+    }
+
+    /**
+     * @Route("/{id}", name="production_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Production $production): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$production->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($production);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('production_index');
     }
 }
