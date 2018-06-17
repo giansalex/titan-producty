ace80b027d9384b4d354cd065aabd3b9fb1bbe90
Giancarlos Salas <giansalex@gmail.com>
2018-06-17 13:05:38 -0500

    add filter history repository

--- src/Repository/HistoryRepository.php
+++ src/Repository/HistoryRepository.php
@@ -3,7 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\History;
+use App\Entity\Material;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
@@ -17,5 +20,17 @@ class HistoryRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, History::class);
+    }
+
+    public function listMaterialByType($type, User $user)
+    {
+        return $this->createQueryBuilder('h')
+            ->select('h.id, h.date, m.id AS ide, m.name, m.code, h.action, h.amount, h.total, u.username')
+            ->innerJoin(Material::class, 'm', Join::WITH, 'h.itemId = m.id')
+            ->innerJoin(User::class, 'u', Join::WITH, 'h.userAction = u.id')
+            ->where('h.user = ?0 AND h.type = ?1')
+            ->setParameters([$user, $type])
+            ->getQuery()
+            ->getResult();
     }
 }
