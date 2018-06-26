665581ff9f60d72418aedac1bf44c78579eaca4b
Giancarlos Salas <giansalex@gmail.com>
2018-06-25 22:24:32 -0500

    add unit list action

---
+++ src/Controller/Api/UnitApiController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giansalex
+ * Date: 25/06/2018
+ * Time: 22:21
+ */
+
+namespace App\Controller\Api;
+
+use App\Repository\UnitRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/unit", options={"expose": true})
+ */
+class UnitApiController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"}, name="unit_api_list")
+     * @param UnitRepository $repository
+     * @return JsonResponse
+     */
+    public function list(UnitRepository $repository): JsonResponse
+    {
+        $items = $repository->findBy(['user' => $this->getUser()]);
+
+        return $this->json($items);
+    }
+}
