c99e8b7efcbf84539dd0de55dc26cbc9cf938b5a
Giancarlos Salas <giansalex@gmail.com>
2018-06-29 00:00:52 -0500

    move index material js to ES6

--- assets/js/app/material/index.controller.js
+++ assets/js/app/material/index.controller.js
@@ -1,43 +1,34 @@
-(function() {
-    'use strict';
+'use strict';
 
-    angular
-        .module('app')
-        .controller('indexMaterial', indexController);
+class MaterialIndexController {
 
-    indexController.$inject = ['materialService'];
-    function indexController($service) {
-        const vm = this;
-        vm.copy = copy;
-        vm.getShowUrl = getShowUrl;
-        vm.getEditUrl = getEditUrl;
-
-        activate();
-
-        function activate() {
-            list();
-        }
-
-        function list() {
-            $service.list()
-                .then(function (r) {
-                    vm.materials = r.data;
-                });
-        }
-
-        function getShowUrl(id) {
-            return Routing.generate('material_show', {id: id});
-        }
-
-        function getEditUrl(id) {
-            return Routing.generate('material_edit', {id: id});
-        }
-
-        function copy(element) {
-            $service.copy(element.id)
-                .then(function (r) {
-                    vm.materials.push(r.data);
-                });
-        }
+    constructor($service, $route) {
+        this.$service = $service;
+        this.$route = $route;
+        this.list();
     }
-})();
+
+    list() {
+        this.$service.list()
+            .then((r) => this.materials = r.data);
+    }
+
+    getShowUrl(id) {
+        return this.$route.generate('material_show', {id: id});
+    }
+
+    getEditUrl(id) {
+        return this.$route.generate('material_edit', {id: id});
+    }
+
+    copy(element) {
+        this.$service.copy(element.id)
+            .then((r) => this.materials.push(r.data));
+    }
+}
+
+MaterialIndexController.$inject = ['materialService', '$route'];
+
+angular
+    .module('app')
+    .controller('indexMaterial', MaterialIndexController);
