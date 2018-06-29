1c48b23d016a53515d3013e3818e3f2dcb956b24
Giancarlos Salas <giansalex@gmail.com>
2018-06-28 23:48:33 -0500

    change material service to ES6

--- assets/js/app/material/material.service.js
+++ assets/js/app/material/material.service.js
@@ -1,38 +1,38 @@
-(function() {
-    'use strict';
+'use strict';
 
-    angular
-        .module('app')
-        .factory('materialService', materialService);
-
-    materialService.$inject = ['$http'];
-    function materialService($http) {
-        return {
-            add: add,
-            get: get,
-            edit: edit,
-            list: list,
-            copy: copy,
-        };
-
-        function get(id) {
-            return $http.get(Routing.generate('material_api_get', {id: id}));
-        }
-
-        function add(material) {
-            return $http.post(Routing.generate('material_api_add'), material);
-        }
-
-        function edit(id, material) {
-            return $http.put(Routing.generate('material_api_edit', {id: id}), material);
-        }
-
-        function list() {
-            return $http.get(Routing.generate('material_api_list'));
-        }
-
-        function copy(id) {
-            return $http.post(Routing.generate('material_api_duplicate', {id: id}));
-        }
+class MaterialService {
+    constructor($http, $route) {
+        this.$http = $http;
+        this.$route = $route;
     }
-})();
+
+    get(id) {
+        return this.$http.get(this.$route.generate('material_api_get', {id: id}));
+    }
+
+    add(material) {
+        return this.$http.post(this.$route.generate('material_api_add'), material);
+    }
+
+    edit(id, material) {
+        return this.$http.put(this.$route.generate('material_api_edit', {id: id}), material);
+    }
+
+    list() {
+        return this.$http.get(this.$route.generate('material_api_list'));
+    }
+
+    copy(id) {
+        return this.$http.post(this.$route.generate('material_api_duplicate', {id: id}));
+    }
+
+    static factory($http, $route) {
+        return new MaterialService($http, $route);
+    }
+}
+
+MaterialService.factory.$inject = ['$http', '$route'];
+
+angular
+    .module('app')
+    .factory('materialService', MaterialService.factory);
