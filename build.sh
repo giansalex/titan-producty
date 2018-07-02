c3dde902874d94d89d81591660c38c322657a6ee
Giancarlos Salas <giansalex@gmail.com>
2018-07-01 22:44:23 -0500

    fix run build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ composer install --no-dev -o
 composer dump-autoload --optimize --no-dev --classmap-authoritative
 composer run routing
 npm run build
-node ci/s3-upload.js --dir=public/build --prefix=minventario/ --skip=manifest.json
+node docker/ci/s3-upload.js --dir=public/build --prefix=minventario/ --skip=manifest.json
 # php ci/publish.php #with manifest.json
